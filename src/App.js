abaf882b4cc1aa540bd2d50e450f38d593b09e88
Atal <atal.atmar@nure.ua>
2021-08-14 22:26:34 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import Login from './Login.jsx'
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
 
+const spotify = new SpotifyWebApi();
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [token, setToken] = useState(null);
+	//Run code based on given condition
+	useEffect(() => {
+		const hash = getTokenFromUrl();
+		window.location.hash = "";
+		const _token = hash.access_token;
+		if (_token) {
+			setToken(_token);
+			spotify.setAccessToken(_token);
+			spotify.getMe().then(user=>{
+				console.log(user);
+			})
+		}
+		else
+			console.log('Token is ->', token);
+	}, [])
+	return (
+		<div className="app">
+			{
+				token ? (
+					<h1>I am logged in</h1>
+				) : (
+						<Login />
+					)
+
+			}
+			
+		</div>
+	);
 }
 
 export default App;
